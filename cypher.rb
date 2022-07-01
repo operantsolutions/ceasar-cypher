42048ce9e287fdb42a10565eeb75bfe3336a71f7
Operant Solutions <106833314+txintegrity@users.noreply.github.com>
2022-07-01 14:56:01 -0400

    Create cypher.rb
    Solved the Ceasar cypher.

---
+++ cypher.rb
@@ -0,0 +1,23 @@
+def ceasar_cypher(string, num)
+  dict = {}
+  ("a".."z").each_with_index do |char, index|
+  dict[char] = index+1
+  end
+  array = string.split("")
+  array.reduce("") do |result, char|
+    char.upcase == char ? up = true : up = false
+    value = dict[char.downcase]
+    new_value = value + num
+    if new_value < 1
+      new_value +=  26
+    elsif new_value > 26
+      new_value -= 26
+    else
+    end
+    if up == true
+      result = result+"#{dict.key(new_value).upcase}"
+    else 
+      result = result+"#{dict.key(new_value)}"
+    end
+  end
+end
